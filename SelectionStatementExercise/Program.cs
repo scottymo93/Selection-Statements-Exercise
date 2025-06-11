3251cd0760e2bface8272714acd9bc1e01cd8eaa
Scott Dyrness <scottdyrness@gmail.com>
2025-06-11 14:44:19 -0700

    completed the selection statement exercise.

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random r = new Random();
+            int favNumber = r.Next(1, 100);
+            //int favNumber = 5;
+            int userInput = 0;
+
+            while (favNumber != userInput)
+            {
+                Console.WriteLine("Guess my favorite number 1-100:");
+                userInput = int.Parse(Console.ReadLine());
+
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine("Too low.");
+                }
+
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine("Too high.");
+                }
+
+                else
+                {
+                    Console.WriteLine("You got my favorite number!");
+                }
+            }
         }
     }
 }
